3395cb89c044fc15a412a3337151b57bab0d5d83
Agung Kurniawan <agung.kurniawan@sehatq.com>
2022-06-02 22:03:54 +0700

    - fix style button

--- resources/views/admin/categories/category/add.blade.php
+++ resources/views/admin/categories/category/add.blade.php
@@ -45,9 +45,9 @@
                <div class="form-group text-center" style="margin-bottom:0%;">
                   <img style="width: 25%;border: 0px solid; border-radius: 10px;" id="viewer" alt=""/>
                </div>
-               <div class="text-center mt-2">
-                  <button type="submit" style="border-radius: 25px;" class="btn btn-primary col-3"> Tambah</button>
-                  <a href="{{ route('admin.master-category') }}" style="border-radius: 25px" class="btn btn-danger col-3"> Kembali</a>
+               <div class="text-right mt-2">
+                  <a href="{{ route('admin.master-category') }}" class="mb-2 mr-2 btn btn-icon btn-light btn-lg"><i class="pe-7s-back btn-icon-wrapper"></i>Back</a>
+                  <button type="submit" class="mb-2 mr-2 btn btn-icon btn-primary btn-lg"><i class="pe-7s-diskette btn-icon-wrapper"></i>Save</button>
                </div>
             </form>
       </div>
