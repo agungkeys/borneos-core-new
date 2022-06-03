ba012114ec2f70df6ea18923c7369206876f277a
Aidil <aidil03f@gmail.com>
2022-06-03 18:41:15 +0800

    sub-category add BC-100

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -172,4 +172,60 @@ class CategoryController extends Controller
         $master_sub_categories = Category::where(['position' => 1])->get();
         return view('admin.categories.sub-category.index', compact('master_sub_categories'));
     }
+    public function master_sub_category_add()
+    {
+        $master_categories = Category::where(['position' => 0])->get();
+        return view('admin.categories.sub-category.add', compact('master_categories'));
+    }
+    public function master_sub_category_store(Request $request)
+    {
+        $request->validate([
+            'category'          => 'required',
+            'sub-category-name' => 'required',
+            'sub-category-slug' => 'required',
+            'image'             => 'image|mimes:jpeg,png,jpg,svg|max:8192'
+        ]);
+        if ($request->file('image')) {
+            $path_name = $request->file('image')->getRealPath();
+            $image = Cloudinary::upload($path_name, ["folder" => "images/categories", "overwrite" => TRUE, "resource_type" => "image"]);
+            $image_url = $image->getSecurePath();
+            $ext = substr($image_url, -3);
+            $ext_jpeg = substr($image_url, -4);
+
+            if ($ext == "jpg") {
+                $image_url_webp = substr($image_url, 0, -3) . "webp";
+            } else if ($ext == "png") {
+                $image_url_webp = substr($image_url, 0, -3) . "webp";
+            } elseif ($ext == "svg") {
+                $image_url_webp = substr($image_url, 0, -3) . "webp";
+            } elseif ($ext_jpeg == "jpeg") {
+                $image_url_webp = substr($image_url, 0, -4) . "webp";
+            };
+
+            $detail_image = [
+                'public_id' =>  $image->getPublicId(),
+                'file_type' =>  $image->getFileType(),
+                'size'      =>  $image->getReadableSize(),
+                'width'     =>  $image->getWidth(),
+                'height'    =>  $image->getHeight(),
+                'extension' =>  $image->getExtension(),
+                'webp'      =>  $image_url_webp
+            ];
+            $additional_image = json_encode($detail_image);
+        } else {
+            $image_url = '';
+            $additional_image = '';
+        };
+        Category::create([
+            'name' => request('sub-category-name'),
+            'slug' => request('sub-category-slug'),
+            'image' => $image_url,
+            'additional_image' => $additional_image,
+            'parent_id' => request('category'),
+            'position'  => 1,
+            'priority'  => 0,
+            'status'    => 1
+        ]);
+        return redirect()->route('admin.master-sub-category');
+    }
 }
