ddf5d7a0bad828bf9e3596a372a30261a5b66a5c
Agung Kurniawan <agung41149@gmail.com>
2022-06-04 14:13:32 +0700

    - update styling for form add/edit

--- resources/views/admin/categories/category/edit.blade.php
+++ resources/views/admin/categories/category/edit.blade.php
@@ -17,40 +17,44 @@
          </div>
       </div>
    </div>
-   <div class="main-card mb-3 card">
-      <div class="card-body">
-            <form action="{{ route('admin.master-category.update',$master_category) }}" method="POST" enctype="multipart/form-data">
-               @method('PUT')
-               @csrf
-               <div class="form-group">
-                  <label for="name">Name</label>
-                  <input type="text" id="name" name="name" value="{{ $master_category->name }}" class="form-control">
-                  @error('name')
-                      <span class="text-danger mt-2">{{ $message }}</span>
-                  @enderror
-               </div>
-                <div class="form-group">
-                  <label for="slug">Slug</label>
-                  <input type="text" id="slug" name="slug" value="{{ $master_category->slug }}" class="form-control">
-                  @error('slug')
-                     <span class="text-danger mt-2">{{ $message }}</span>
-                  @enderror
-               </div>
-                <div class="form-group">
-                  <label for="image">Image</label><br>
-                  <input type="file" accept="image/*" id="image" name="image">
-                  @error('image')
-                      <br><span class="text-danger mt-2">{{ $message }}</span>
-                  @enderror
-               </div>
-               <div class="form-group text-center" style="margin-bottom:0%;">
-                  <img style="width: 25%;border: 0px solid; border-radius: 10px;" id="viewer" src="{{ URL::to($master_category->image) }}" alt=""/>
-               </div>
-               <div class="text-center mt-2">
-                  <button type="submit" style="border-radius: 25px;" class="btn btn-warning col-3"> Update</button>
-                  <a href="{{ route('admin.master-category') }}" style="border-radius: 25px" class="btn btn-danger col-3"> Back</a>
-               </div>
-            </form>
+   <div class="row">
+      <div class="col-md-6">
+         <div class="main-card mb-3 card">
+            <div class="card-body">
+               <form action="{{ route('admin.master-category.update',$master_category) }}" method="POST" enctype="multipart/form-data">
+                  @method('PUT')
+                  @csrf
+                  <div class="form-group">
+                     <label for="name">Name</label>
+                     <input type="text" id="name" name="name" value="{{ $master_category->name }}" class="form-control">
+                     @error('name')
+                           <span class="text-danger mt-2">{{ $message }}</span>
+                     @enderror
+                  </div>
+                     <div class="form-group">
+                     <label for="slug">Slug</label>
+                     <input type="text" id="slug" name="slug" value="{{ $master_category->slug }}" class="form-control">
+                     @error('slug')
+                        <span class="text-danger mt-2">{{ $message }}</span>
+                     @enderror
+                  </div>
+                     <div class="form-group">
+                     <label for="image">Image</label><br>
+                     <input type="file" accept="image/*" id="image" name="image">
+                     @error('image')
+                           <br><span class="text-danger mt-2">{{ $message }}</span>
+                     @enderror
+                  </div>
+                  <div class="form-group text-center" style="margin-bottom:0%;">
+                     <img style="width: 25%;border: 0px solid; border-radius: 10px;" id="viewer" src="{{ URL::to($master_category->image) }}" alt=""/>
+                  </div>
+                  <div class="text-right mt-2">
+                     <a href="{{ route('admin.master-category') }}" class="mb-2 mr-2 btn btn-icon btn-light btn-lg"><i class="pe-7s-back btn-icon-wrapper"></i>Back</a>
+                     <button type="submit" class="mb-2 mr-2 btn btn-icon btn-primary btn-lg"><i class="pe-7s-diskette btn-icon-wrapper"></i>Update</button>
+                  </div>
+               </form>
+            </div>
+         </div>
       </div>
    </div>
      <script>
