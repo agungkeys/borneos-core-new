017bbec230917f20fbf8d02fc6bba18632ad14ee
Aidil <aidil03f@gmail.com>
2022-06-04 10:33:18 +0800

    edit update sub-category BC-101

--- resources/views/admin/categories/sub-category/index.blade.php
+++ resources/views/admin/categories/sub-category/index.blade.php
@@ -42,7 +42,7 @@
                         <td>{{ $master_sub_category->slug }}</td>
                         <td>
                            <form action="{{ route('admin.master-category.delete',$master_sub_category->id) }}" method="post">
-                              <a href="{{ route('admin.master-category.edit',$master_sub_category->id) }}" class="btn btn-warning btn-sm"> Edit</a>
+                              <a href="{{ route('admin.master-sub-category.edit',$master_sub_category->id) }}" class="btn btn-warning btn-sm"> Edit</a>
                               @method('delete')
                               @csrf
                               <button type="submit" onclick="return confirm('Are you sure ?')" class="btn btn-danger btn-sm"> Delete</button>
