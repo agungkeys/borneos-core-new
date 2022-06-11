dc69adb4d783efcd993bb000aaef6b8978b38a42
Aidil <aidil03f@gmail.com>
2022-06-11 10:46:24 +0800

    switch on/off categories BC-156

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,11 +10,23 @@ class Category extends Model
 {
     use HasFactory;
     use Sortable;
-    
+
     public $sortable = [
         'id', 'name', 'slug'
     ];
     protected $fillable = [
         'name', 'slug', 'image', 'additional_image', 'parent_id', 'position', 'priority', 'status'
     ];
+
+    public function CategoryNameFromSubCategory()
+    {
+        $categories = Category::where(['id' => $this->parent_id])->get('name');
+        foreach ($categories as $category) {
+            return $category->name;
+        };
+    }
+    public function CategoryNameFromSubSubCategory()
+    {
+        return $this->CategoryNameFromSubCategory();
+    }
 }
