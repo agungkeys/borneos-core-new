37a1cf82c683f943e44035a62871232cabe1f3ed
Aidil <aidil03f@gmail.com>
2022-06-12 16:20:13 +0800

    Page User Add BC-159

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -95,4 +95,124 @@ class UserController extends Controller
         Alert::success('Success', 'Created Successfully');
         return redirect()->route('admin.master-user');
     }
+    public function master_user_edit($id)
+    {
+        return view('admin.user.edit', [
+            'admin_roles'  => AdminRole::all(),
+            'master_users' => Admin::find($id)
+        ]);
+    }
+    public function master_user_update(Request $request, $id)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'role'       => 'required',
+            'phone'      => 'required|numeric',
+            'email'      => 'required',
+            'image'      => 'image|mimes:jpeg,png,jpg,svg|max:8192',
+            'password_confirmation' => ['same:password']
+        ]);
+        $admin = Admin::findOrFail($id);
+        if ($request->file('image')) {
+            if ($admin->image) {
+                if (!$admin->additional_image) {
+                    $path_name = $request->file('image')->getRealPath();
+                    $image = Cloudinary::upload($path_name, ["folder" => "images/users", "overwrite" => TRUE, "resource_type" => "image"]);
+                    $image_url = $image->getSecurePath();
+                    $ext = substr($image_url, -3);
+                    $ext_jpeg = substr($image_url, -4);
+
+                    if ($ext == "jpg") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } else if ($ext == "png") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } elseif ($ext == "svg") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } elseif ($ext_jpeg == "jpeg") {
+                        $image_url_webp = substr($image_url, 0, -4) . "webp";
+                    };
+                    $detail_image = [
+                        'public_id' =>  $image->getPublicId(),
+                        'file_type' =>  $image->getFileType(),
+                        'size'      =>  $image->getReadableSize(),
+                        'width'     =>  $image->getWidth(),
+                        'height'    =>  $image->getHeight(),
+                        'extension' =>  $image->getExtension(),
+                        'webp'      =>  $image_url_webp
+                    ];
+                    $additional_image = json_encode($detail_image);
+                } else {
+                    $key = json_decode($admin->additional_image);
+                    Cloudinary::destroy($key->public_id);
+                    $path_name = $request->file('image')->getRealPath();
+                    $image = Cloudinary::upload($path_name, ["folder" => "images/users", "overwrite" => TRUE, "resource_type" => "image"]);
+                    $image_url = $image->getSecurePath();
+                    $ext = substr($image_url, -3);
+                    $ext_jpeg = substr($image_url, -4);
+
+                    if ($ext == "jpg") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } else if ($ext == "png") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } elseif ($ext == "svg") {
+                        $image_url_webp = substr($image_url, 0, -3) . "webp";
+                    } elseif ($ext_jpeg == "jpeg") {
+                        $image_url_webp = substr($image_url, 0, -4) . "webp";
+                    };
+
+                    $detail_image = [
+                        'public_id' =>  $image->getPublicId(),
+                        'file_type' =>  $image->getFileType(),
+                        'size'      =>  $image->getReadableSize(),
+                        'width'     =>  $image->getWidth(),
+                        'height'    =>  $image->getHeight(),
+                        'extension' =>  $image->getExtension(),
+                        'webp'      =>  $image_url_webp
+                    ];
+                    $additional_image = json_encode($detail_image);
+                }
+            } else {
+                $path_name = $request->file('image')->getRealPath();
+                $image = Cloudinary::upload($path_name, ["folder" => "images/users", "overwrite" => TRUE, "resource_type" => "image"]);
+                $image_url = $image->getSecurePath();
+                $ext = substr($image_url, -3);
+                $ext_jpeg = substr($image_url, -4);
+
+                if ($ext == "jpg") {
+                    $image_url_webp = substr($image_url, 0, -3) . "webp";
+                } else if ($ext == "png") {
+                    $image_url_webp = substr($image_url, 0, -3) . "webp";
+                } elseif ($ext == "svg") {
+                    $image_url_webp = substr($image_url, 0, -3) . "webp";
+                } elseif ($ext_jpeg == "jpeg") {
+                    $image_url_webp = substr($image_url, 0, -4) . "webp";
+                };
+                $detail_image = [
+                    'public_id' =>  $image->getPublicId(),
+                    'file_type' =>  $image->getFileType(),
+                    'size'      =>  $image->getReadableSize(),
+                    'width'     =>  $image->getWidth(),
+                    'height'    =>  $image->getHeight(),
+                    'extension' =>  $image->getExtension(),
+                    'webp'      =>  $image_url_webp
+                ];
+                $additional_image = json_encode($detail_image);
+            }
+        } else {
+            $image_url = $admin->image;
+            $additional_image = $admin->additional_image;
+        };
+        $admin->update([
+            'f_name'     => $request->first_name,
+            'l_name'     => $request->last_name,
+            'role_id'    => $request->role,
+            'phone'      => $request->phone,
+            'email'      => $request->email,
+            'image'      => $image_url,
+            'password'   => $request->password ? bcrypt($request->password) : $admin->password,
+            'additional_image' => $additional_image,
+        ]);
+        Alert::success('Updated', 'Data Updated Successfully');
+        return redirect()->route('admin.master-user');
+    }
 }
