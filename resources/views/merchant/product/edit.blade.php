1e894b4631ce4670d6f4e8ce49ddc670dfe47480
Aidil <aidil03f@gmail.com>
2022-06-28 19:21:31 +0800

    Edit Merchant Product BC-196

--- resources/views/merchant/product/edit.blade.php
+++ resources/views/merchant/product/edit.blade.php
@@ -9,7 +9,7 @@
                <i class="pe-7s-medal icon-gradient bg-tempting-azure"></i>
             </div>
             <div>
-               Add Master Product
+               Edit Master Product
                <div class="page-title-subheading">
 
                </div>
@@ -20,20 +20,21 @@
 
    <div class="main-card mb-3 card">
       <div class="card-body">
-         <form action="{{ route('merchant.master-product.store') }}" method="POST" enctype="multipart/form-data" autocomplete="off">
+         <form action="{{ route('merchant.master-product.update',$product->id) }}" method="POST" enctype="multipart/form-data">
+            @method('PUT')
             @csrf
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="merchant_id">Merchant</label>
-                        <input type="hidden" id="merchant_id" name="merchant_id" value="{{ $merchant->id }}">
-                        <input type="text" value="{{$merchant->name}}" class="form-control" disabled>
+                        <input type="hidden" id="merchant_id" name="merchant_id" value="{{ $product->merchant->id }}">
+                        <input type="text" value="{{$product->merchant->name}}" class="form-control" disabled>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="product_name">Product Name</label>
-                        <input type="text" id="product_name" name="product_name" class="form-control" placeholder="Product Name">
+                        <input type="text" id="product_name" name="product_name" value="{{ $product->name }}" class="form-control">
                         @error('product_name')
                             <span class="text-danger mt-2">{{ $message }}</span>
                         @enderror
@@ -44,7 +45,7 @@
                 <div class="col-md-4">
                     <div class="form-group">
                        <label for="price">Price</label>
-                        <input type="number" name="price" id="price" class="form-control" placeholder="Price">
+                        <input type="number" name="price" id="price" value="{{ substr($product->price, 0, -3) }}" class="form-control">
                         @error('price')
                             <span class="text-danger mt-2">{{ $message }}</span>
                         @enderror
@@ -54,9 +55,9 @@
                     <div class="form-group">
                        <label for="discount_type">Discount Type</label>
                         <select class="multiselect-dropdown form-control" name="discount_type" id="discount_type">
-                            <option selected value="">Choose One!</option>
-                            <option value="percent">Percent</option>
-                            <option value="amount">Amount</option>
+                            <option selected value=""></option>
+                            <option {{ $product->discount_type == 'percent' ? 'selected':'' }} value="percent">Percent</option>
+                            <option {{ $product->discount_type == 'amount' ? 'selected':'' }} value="amount">Amount</option>
                         </select>
                         @error('discount_type')
                             <span class="text-danger mt-2">{{ $message }}</span>
@@ -66,7 +67,7 @@
                 <div class="col-md-4">
                     <div class="form-group">
                        <label for="discount">Discount</label>
-                       <input type="number" name="discount" id="discount" class="form-control" placeholder="Discount">
+                       <input type="number" name="discount" id="discount" value="{{ $product->discount == 0 ? '' : substr($product->discount, 0, -3) }}" class="form-control">
                     </div>
                     @error('discount')
                         <span class="text-danger mt-2">{{ $message }}</span>
@@ -84,8 +85,8 @@
                 <div class="col-md-4">
                     <div class="form-group">
                        <label for="sub_category">Sub Category</label>
-                        <select class="multiselect-dropdown form-control" name="sub_category" id="sub_category" onchange="get_sub_category(this.value)" required>
-                            <option disabled selected value="">Choose One!</option>
+                        <select class="multiselect-dropdown form-control" name="sub_category" id="sub_category" onchange="handleSubCategory(this.value)" required>
+            
                         </select>
                     </div>
                 </div>
@@ -93,7 +94,7 @@
                     <div class="form-group">
                        <label for="sub_sub_category">Sub Sub Category</label>
                         <select class="multiselect-dropdown form-control" name="sub_sub_category" id="sub_sub_category">
-                            <option disabled selected value="">Choose One!</option>
+                            
                         </select>
                     </div>
                 </div>
@@ -102,21 +103,21 @@
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="available_time_starts">Available time starts</label>
-                        <input type="time" name="available_time_starts" class="form-control" id="available_time_starts" placeholder="Ex : 08:30 am" required>
+                        <input type="time" name="available_time_starts" class="form-control" id="available_time_starts" value="{{ $product->available_time_starts }}" required>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group">
                         <label for="available_time_ends">Available time ends</label>
-                        <input type="time" name="available_time_ends" class="form-control" id="available_time_ends" placeholder="Ex : 10:30 am" required>
+                        <input type="time" name="available_time_ends" class="form-control" id="available_time_ends" value="{{ $product->available_time_ends }}" required>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea type="text" name="description" class="form-control" placeholder="Description"></textarea>
+                        <label for="description">Short Description</label>
+                        <textarea type="text" name="description" class="form-control">{{ $product->description }}</textarea>
                     </div>
                 </div>
             </div>
@@ -128,11 +129,11 @@
                @enderror
             </div>
             <div class="form-group text-center" style="margin-bottom:0%;">
-               <img style="width: 25%;border: 0px solid; border-radius: 10px;" id="viewer" alt=""/>
+               <img style="width: 25%;border: 0px solid; border-radius: 10px;" id="viewer" src="{{ URL::to($product->image) }}" alt=""/>
             </div>
             <div class="text-right mt-2">
-                <a href="{{ route('merchant.master-product') }}" class="mb-2 mr-2 btn btn-icon btn-light btn-lg"><i class="pe-7s-back btn-icon-wrapper"></i>Back</a>
-                <button type="submit" class="mb-2 mr-2 btn btn-icon btn-primary btn-lg"><i class="pe-7s-diskette btn-icon-wrapper"></i>Save</button>
+               <a href="{{ route('merchant.master-product') }}" class="mb-2 mr-2 btn btn-icon btn-light btn-lg"><i class="pe-7s-back btn-icon-wrapper"></i>Back</a>
+               <button type="submit" class="mb-2 mr-2 btn btn-icon btn-primary btn-lg"><i class="pe-7s-diskette btn-icon-wrapper"></i>Update</button>
             </div>
          </form>
       </div>
@@ -140,27 +141,50 @@
 @endsection
 @section('js')
     <script>
-        $.get('/get-merchants/{{$merchant->id}}',function(response){
-            $('#category').val(response.category);
-            $('#category_name').val(response.category_name);
-            $.each(response.sub_categories, function (i, item) {
-                $('#sub_category').append($('<option>', {
-                    value: item.id,
-                    text : item.name
-                }));
-            });
-        });
-        function get_sub_category(id){
-            $.get('/get-sub-sub-category/'+id,function(response){
-                $('#sub_sub_category').empty();
-                $.each(response, function (i, item) {
-                    $('#sub_sub_category').append($('<option>', {
-                        value: item.id,
-                        text : item.name
-                    }));
+        function handleSubCategory(id){
+            if(!id){return;}else{
+                $("#sub_sub_category").empty();
+                $.get(`/get-sub-sub-category/${id}`,function(response){
+                     $.each(response, function (i, item) {
+                         $('#sub_sub_category').append($("<option>", {
+                             value: item.id,
+                             text : item.name
+                         }));
+                     });
                 });
+            }
+         };
+         $(document).ready(function(){
+            $.get('/get-merchants/{{ $product->merchant_id }}',function(response){
+                $('#category').val(response.category);
+                $('#category_name').val(response.category_name);
             });
-        }
+
+            $.get('/get-sub-category/{{ $category_id }}',function(response){
+              if(response){
+                $.each(response, function (i, item) {
+                  $('#sub_category').append($("<option>", {
+                      value: item.id,
+                      text : item.name
+                  }));
+                });
+                $('#sub_category').val('{{ $sub_category_id }}');
+                $('#sub_category').trigger('change');
+              }
+            });
+            
+            $.get('/get-sub-sub-category/{{ $sub_category_id }}',function(response){
+                $.each(response, function (i, item) {
+                  $('#sub_sub_category').append($("<option>", {
+                      value: item.id,
+                      text : item.name
+                  }));
+                });
+                $('#sub_sub_category').val('{{ $sub_sub_category_id }}');
+                $('#sub_sub_category').trigger('change');
+            });
+            $('#sub_category').val('{{ $sub_sub_category_id }}').trigger('change');
+        });
         function readURL(input) {
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
