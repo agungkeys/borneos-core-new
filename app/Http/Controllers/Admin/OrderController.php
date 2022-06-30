7c48f11fcf6c1f712fb22fcd5a50685cbce0c21d
Aidil <aidil03f@gmail.com>
2022-06-30 20:42:50 +0800

    fix order list BC-130

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,7 +10,31 @@ class OrderController extends Controller
 {
     public function index(Request $request, $status)
     {
+        $filter = $request->query('filter');
+        if (!empty($filter)) {
+            $orders = Order::sortable()
+                ->where('orders.customer_name', 'like', '%' . $filter . '%')
+                ->orWhere('orders.id', 'like', '%' . $filter . '%')
+                ->orWhere('orders.order_type', 'like', '%' . $filter . '%')
+                ->orWhere('orders.customer_telp', 'like', '%' . $filter . '%')
+                ->orWhere('orders.customer_address', 'like', '%' . $filter . '%')
+                ->orWhere('orders.customer_notes', 'like', '%' . $filter . '%')
+                ->orWhere('orders.total_item', 'like', '%' . $filter . '%')
+                ->orWhere('orders.total_item_price', 'like', '%' . $filter . '%')
+                ->orWhere('orders.total_distance_price', 'like', '%' . $filter . '%')
+                ->orWhere('orders.total_price', 'like', '%' . $filter . '%')
+                ->orWhere('orders.payment_type', 'like', '%' . $filter . '%')
+                ->orWhere('orders.payment_status', 'like', '%' . $filter . '%')
+                ->orWhere('orders.status', 'like', '%' . $filter . '%')
+                ->paginate(10);
+        } else {
+            $orders = Order::sortable()->where('orders.status', 'like', '%' . $status . '%')->paginate(10);
+        }
+        return view('admin.orders.index', compact('orders', 'filter'));
+    }
 
+    public function all(Request $request)
+    {
         $filter = $request->query('filter');
         if (!empty($filter)) {
             $orders = Order::sortable()
@@ -32,30 +56,5 @@ class OrderController extends Controller
             $orders = Order::sortable()->paginate(10);
         }
         return view('admin.orders.index', compact('orders', 'filter'));
-    }
-
-    public function all(Request $request)
-    {
-      $filter = $request->query('filter');
-      if (!empty($filter)) {
-          $orders = Order::sortable()
-              ->where('orders.customer_name', 'like', '%' . $filter . '%')
-              ->orWhere('orders.id', 'like', '%' . $filter . '%')
-              ->orWhere('orders.order_type', 'like', '%' . $filter . '%')
-              ->orWhere('orders.customer_telp', 'like', '%' . $filter . '%')
-              ->orWhere('orders.customer_address', 'like', '%' . $filter . '%')
-              ->orWhere('orders.customer_notes', 'like', '%' . $filter . '%')
-              ->orWhere('orders.total_item', 'like', '%' . $filter . '%')
-              ->orWhere('orders.total_item_price', 'like', '%' . $filter . '%')
-              ->orWhere('orders.total_distance_price', 'like', '%' . $filter . '%')
-              ->orWhere('orders.total_price', 'like', '%' . $filter . '%')
-              ->orWhere('orders.payment_type', 'like', '%' . $filter . '%')
-              ->orWhere('orders.payment_status', 'like', '%' . $filter . '%')
-              ->orWhere('orders.status', 'like', '%' . $filter . '%')
-              ->paginate(10);
-      } else {
-          $orders = Order::sortable()->paginate(10);
-      }
-      return view('admin.orders.index', compact('orders', 'filter'));
     }
 }
