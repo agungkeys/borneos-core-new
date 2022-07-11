f13fbfd919e82b106303461a30ac23284a123488
Aidil <aidil03f@gmail.com>
2022-07-11 13:53:09 +0800

    [Admin][Dashboard] Calculate Order New, Process, Otw, Delivered, All BC-247

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,17 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Product;
-use App\Models\Merchant;
-use App\Models\User;
-use Carbon\Carbon;
+use App\Models\Order;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
   public function dashboard(Request $request)
   {
-    return view('admin.dashboard');
+    $ordersCount = (object)[
+      'new'        => Order::where(['status' => 'new'])->count(),
+      'processing' => Order::where(['status' => 'processing'])->count(),
+      'otw'        => Order::where(['status' => 'otw'])->count(),
+      'delivered'  => Order::where(['status' => 'delivered'])->count(),
+      'cancel'     => Order::where(['status' => 'cancel'])->count(),
+      'all'        => Order::count()
+    ];
+    return view('admin.dashboard', compact('ordersCount'));
   }
 }
