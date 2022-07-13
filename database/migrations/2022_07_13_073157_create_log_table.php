bbc9dfa4592c503bb9a99db40858b5e0f1f07164
Aidil <aidil03f@gmail.com>
2022-07-13 15:38:40 +0800

    Schema DB log table BC-245

---
+++ database/migrations/2022_07_13_073157_create_log_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->id();
+            $table->string('prefix')->nullable();
+            $table->string('method', 100);
+            $table->string('method_detail', 100);
+            $table->text('value');
+            $table->text('description')->nullable();
+            $table->string('user');
+            $table->string('user_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+}
