357c933f68841aa7f6ef30c3897a01b588f2f7a7
Aidil <aidil03f@gmail.com>
2022-08-20 09:21:44 +0800

    API cart-validation bc-368

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -26,4 +26,5 @@ Route::group(['namespace' => 'Api\V1'], function () {
   Route::get('/generate-slug-product', 'ProductController@generate_slug_products');
   Route::get('/product-list-merchant-landing/{slug}', 'ProductController@get_product_list_merchant_landing');
   Route::get('/product-recomendations', 'ProductController@get_product_recomendation');
+  Route::post('/cart-validation', 'ProductController@cart_validation');
 });
