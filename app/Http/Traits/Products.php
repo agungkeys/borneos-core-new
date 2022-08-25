c9a9f3f054caadfc3ee3af165691df3308120142
Aidil <aidil03f@gmail.com>
2022-08-25 16:33:42 +0800

    API product detail

--- app/Http/Traits/Products.php
+++ app/Http/Traits/Products.php
@@ -334,6 +334,54 @@ trait Products
         };
         return $result;
     }
+    public function result_product_detail($data)
+    {
+        foreach ($data as $product) {
+            return  [
+                'id' => $product->id,
+                'merchantId' => [
+                    'id' => $product->merchant->id,
+                    'name' => $product->merchant->name,
+                    'slug' => $product->merchant->slug
+                ],
+                'name' => $product->name,
+                'slug' => $product->slug,
+                'description' => $product->description,
+                'image' => $product->image,
+                'additionalImage' => json_decode($product->additional_image),
+                'categoryId' => [
+                    'id' => $product->category_id,
+                    'name' => $product->category_id ? $product->category->name : '',
+                    'slug' => $product->category_id ? $product->category->slug : ''
+                ],
+                'subCategoryId' => [
+                    'id' => $product->sub_category_id,
+                    'name' => $product->sub_category_id ? $product->SubCategory->name : '',
+                    'slug' => $product->sub_category_id ? $product->SubCategory->slug : ''
+                ],
+                'subSubCategoryId' => [
+                    'id' => $product->sub_sub_category_id,
+                    'name' => $product->sub_sub_category_id ? $product->SubSubCategory->name : '',
+                    'slug' => $product->sub_sub_category_id ? $product->SubSubCategory->slug : ''
+                ],
+                'price' => number_format($product->price, 2, ",", "."),
+                'taxType' => $product->tax_type,
+                'discount' => $product->discount,
+                'discountType' => $product->discount_type,
+                'discountPrice' => $this->discountPriceOnProduct([
+                    'discount' => $product->discount,
+                    'discount_type' => $product->discount_type,
+                    'price' => number_format($product->price, 0, ',', '')
+                ]),
+                'availableTimeStarts' => $product->available_time_starts,
+                'availableTimeEnds' => $product->available_time_ends,
+                'setMenu' => $product->set_menu,
+                'status' => $product->status,
+                'orderCount' => $product->order_count
+            ];
+        };
+    }
+
     public function processGenerateSlug($data)
     {
         $slug = Str::slug($data);
