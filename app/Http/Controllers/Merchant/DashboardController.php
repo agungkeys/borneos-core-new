60720616e05fddde4598b2b6e36834b577906258
fsvhino <ferrysafrizal20@gmail.com>
2022-10-16 16:47:56 +0800

    BC-486 [Task][Merchant][Dashboard] BugFix Total Order, if status order = paid & status 'not new'

--- app/Http/Controllers/Merchant/DashboardController.php
+++ app/Http/Controllers/Merchant/DashboardController.php
@@ -9,21 +9,21 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-  public function dashboard(Request $request)
-  {
-    $merchant   = Merchant::where(['vendor_id' => Auth()->id()])->get();
-    $merchantAll = Merchant::all()->count();
-    $productCount = Product::where(['merchant_id' => $merchant[0]->id])->count();
-    $dateNow = Carbon::today();
-    $orderCount = (object)[
-      'new'        => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'new'])->count(),
-      'processing' => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'processing'])->count(),
-      'otw'        => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'otw'])->count(),
-      'delivered'  => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'delivered'])->count(),
-      'cancel'     => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'cancel'])->count(),
-      'all'        => Order::where(['merchant_id' => $merchant[0]->id])->count(),
-      'today'      => Order::where(['merchant_id' => $merchant[0]->id])->whereDate( 'created_at', $dateNow )->count()
-    ];
-    return view('merchant.dashboard', compact('merchant', 'orderCount', 'merchantAll', 'productCount', 'dateNow'));
-  }
+    public function dashboard(Request $request)
+    {
+        $merchant   = Merchant::where(['vendor_id' => Auth()->id()])->get();
+        $merchantAll = Merchant::all()->count();
+        $productCount = Product::where(['merchant_id' => $merchant[0]->id])->count();
+        $dateNow = Carbon::today();
+        $orderCount = (object)[
+            'new'        => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'new'])->count(),
+            'processing' => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'processing'])->count(),
+            'otw'        => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'otw'])->count(),
+            'delivered'  => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'delivered'])->count(),
+            'cancel'     => Order::where(['merchant_id' => $merchant[0]->id, 'status' => 'cancel'])->count(),
+            'all'        => Order::where(['merchant_id' => $merchant[0]->id, 'payment_status' => 'paid'])->count(),
+            'today'      => Order::where(['merchant_id' => $merchant[0]->id, 'payment_status' => 'paid'])->whereDate('created_at', $dateNow)->count()
+        ];
+        return view('merchant.dashboard', compact('merchant', 'orderCount', 'merchantAll', 'productCount', 'dateNow'));
+    }
 }
