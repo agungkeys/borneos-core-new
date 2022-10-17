a76bf2e433fb68c42e4e81385db0d0ad29c254c1
aidil03f <aidil03f@gmail.com>
2022-10-17 20:10:11 +0700

    borneos-core bc-488

--- app/Http/Controllers/Merchant/BannerController.php
+++ app/Http/Controllers/Merchant/BannerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Merchant;
 
 use App\Http\Controllers\Controller;
+use App\Http\Traits\CloudinaryImage;
 use App\Models\Banner;
 use App\Models\Merchant;
 use Illuminate\Http\Request;
@@ -12,6 +13,8 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class BannerController extends Controller
 {
+    use CloudinaryImage;
+
     public function master_banner_index(Request $request){
         $filter = $request->query('filter');
         $merchant = Merchant::where(['vendor_id' => Auth::guard('merchant')->user()->id])->first();
@@ -27,12 +30,11 @@ class BannerController extends Controller
         return view('merchant.banner.index', compact('banners', 'filter'));
     }
 
-    public function master_banner_status(Request $request){
+    public function master_banner_status(Request $request)
+    {
         $banner = Banner::withoutGlobalScopes()->find($request->id);
-
         $banner->status = $request->status;
         $banner->save();
-
         Alert::toast('Status updated!', 'success');
         return redirect()->route('merchant.master-banner');
     }
@@ -41,61 +43,41 @@ class BannerController extends Controller
         return view('merchant.banner.add');
     }
 
-    public function master_banner_store(Request $request){
-        $merchant = Merchant::where(['vendor_id' => Auth::guard('merchant')->user()->id])->first();
-
+    public function master_banner_store(Request $request)
+    {
         $request->validate([
             'title' => 'required',
-            'type' => 'required',
+            'type'  => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,svg|max:8192',
-            'url' => 'nullable',
+            'url'   => 'nullable',
         ]);
-
+        
         if ($request->file('image')) {
-            $path_name = $request->file('image')->getRealPath();
-            $image = Cloudinary::upload($path_name, ["folder" => "images/banners", "overwrite" => TRUE, "resource_type" => "image"]);
-            $image_url = $image->getSecurePath();
-            $ext = substr($image_url, -3);
-            $ext_jpeg = substr($image_url, -4);
-
-            if ($ext == "jpg") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } else if ($ext == "png") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } elseif ($ext == "svg") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } elseif ($ext_jpeg == "jpeg") {
-                $image_url_webp = substr($image_url, 0, -4) . "webp";
-            };
-
-            $detail_image = [
-                'public_id' =>  $image->getPublicId(),
-                'file_type' =>  $image->getFileType(),
-                'size'      =>  $image->getReadableSize(),
-                'width'     =>  $image->getWidth(),
-                'height'    =>  $image->getHeight(),
-                'extension' =>  $image->getExtension(),
-                'webp'      =>  $image_url_webp
-            ];
+            $image = $this->UploadImageCloudinary(['image' => $request->file('image'), 'folder' => 'images/banners']);
+            $image_url = $image['url'];
+            $additional_image = $image['additional_image'];
         } else {
             $image_url = '';
+            $additional_image = '';
         };
+        
+        $merchant = Merchant::where(['vendor_id' => Auth::guard('merchant')->user()->id])->first();
 
         $banner = new Banner();
         $banner->title = $request->title;
         $banner->type = $request->type;
         $banner->image = $image_url;
+        $banner->additional_image = $additional_image;
         $banner->url = $request->url;
         $banner->status = 1;
         $banner->merchant_id = $merchant->id;
-        $banner->admin_id = $merchant->id;
-
         $banner->save();
-        Alert::success('Success', 'Data saved succesfully!');
+        Alert::success('Success', 'Data saved succesfully');
         return redirect()->route('merchant.master-banner');
     }
 
-    public function master_banner_edit($id){
+    public function master_banner_edit($id)
+    {
         $merchant = Merchant::where(['vendor_id' => Auth::guard('merchant')->user()->id])->first();
         $banner = Banner::where([['merchant_id', '=', $merchant->id], ['id', '=', $id]])->first();
 
@@ -108,55 +90,49 @@ class BannerController extends Controller
         }
     }
 
-    public function master_banner_update(Request $request, $id){
+    public function master_banner_update(Request $request, int $id)
+    {
+        $request->validate([
+            'title' => 'required',
+            'type'  => 'sometimes',
+            'image' => 'image|mimes:jpeg,png,jpg,svg|max:8192',
+            'url'   => 'sometimes',
+        ]);
+
         $banner = Banner::findOrFail($id);
 
         if ($request->file('image')) {
-            $path_name = $request->file('image')->getRealPath();
-            $image = Cloudinary::upload($path_name, ["folder" => "images/banners", "overwrite" => TRUE, "resource_type" => "image"]);
-            $image_url = $image->getSecurePath();
-            $ext = substr($image_url, -3);
-            $ext_jpeg = substr($image_url, -4);
-
-            if ($ext == "jpg") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } else if ($ext == "png") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } elseif ($ext == "svg") {
-                $image_url_webp = substr($image_url, 0, -3) . "webp";
-            } elseif ($ext_jpeg == "jpeg") {
-                $image_url_webp = substr($image_url, 0, -4) . "webp";
-            };
-
-            $detail_image = [
-                'public_id' =>  $image->getPublicId(),
-                'file_type' =>  $image->getFileType(),
-                'size'      =>  $image->getReadableSize(),
-                'width'     =>  $image->getWidth(),
-                'height'    =>  $image->getHeight(),
-                'extension' =>  $image->getExtension(),
-                'webp'      =>  $image_url_webp
-            ];
-        } else {
+            $image = $this->UpdateImageCloudinary([
+                'image'      => $request->file('image'),
+                'folder'     => 'images/banners',
+                'collection' => $banner
+            ]);
+            $image_url = $image['url'];
+            $additional_image = $image['additional_image'];
+        }else {
             $image_url = $banner->image;
-        };
+            $additional_image = $banner->additional_image;
+        }
 
         $banner->title = $request->title;
         $banner->type = $banner->type;
         $banner->image = $image_url;
+        $banner->additional_image = $additional_image;
         $banner->url = $request->url;
         $banner->status = $banner->status;
         $banner->merchant_id = $banner->merchant_id;
-        $banner->admin_id = $banner->admin_id;
-
         $banner->save();
-        Alert::success('Success', 'Data updated succesfully!');
+        Alert::success('Success', 'Data updated succesfully');
         return redirect()->route('merchant.master-banner');
     }
 
-    public function master_banner_delete($id){
+    public function master_banner_delete(int $id)
+    {
         $banner = Banner::findOrFail($id);
-
+         if (substr($banner->image, 0, 4) == 'http') {
+            $key = json_decode($banner->additional_image);
+            Cloudinary::destroy($key->public_id);
+        }
         $banner->delete();
         return response()->json(['status' => 200]);
     }
