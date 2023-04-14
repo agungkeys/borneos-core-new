77afeda096be0da27aa559028fb9eb4e15bce845
Agung Kurniawan <agung41149@gmail.com>
2023-04-14 20:16:43 +0800

    - fix banget ticket detail api

--- app/Http/Traits/Travels.php
+++ app/Http/Traits/Travels.php
@@ -16,30 +16,28 @@ trait Travels
         if ($travelDetail->count() == 0) {
             return null;
         } else {
-            foreach ($travelDetail as $travel) {
-                $results[] = [
-                    'id' => $travel->id,
-                    'prefix' => $travel->prefix,
-                    'fullname' => $travel->fullname,
-                    'telp' => $travel->telp,
-                    'fullAddress' => $travel->full_address,
-                    'subDistrict' => $travel->sub_district,
-                    'district' => $travel->district,
-                    'route' => $travel->route,
-                    'seatNo' => $travel->seat_no,
-                    'ktp' => $travel->ktp,
-                    'kk' => $travel->kk,
-                    'vaccine' => $travel->vaccine,
-                    'urlIdCard' => $travel->url_idcard,
-                    'urlIdVaccine' => $travel->url_idvaccine,
-                    'idCardNo' => $travel->id_card_no,
-                    'approvedAt' => $travel->approved_at ? $travel->approved_at->format('d/m/Y') : null,
-                    'createdAt' => $travel->created_at->format('d/m/Y'),
-                    'updatedAt' => $travel->updated_at->format('d/m/Y'),
-                    'deletedAt' => $travel->deleted_at ?  $travel->deleted_at->format('d/m/Y') : null,
-                ];
-                return $results;
-            };
+            $results[] = [
+                'id' => $travel->id,
+                'prefix' => $travel->prefix,
+                'fullname' => $travel->fullname,
+                'telp' => $travel->telp,
+                'fullAddress' => $travel->full_address,
+                'subDistrict' => $travel->sub_district,
+                'district' => $travel->district,
+                'route' => $travel->route,
+                'seatNo' => $travel->seat_no,
+                'ktp' => $travel->ktp,
+                'kk' => $travel->kk,
+                'vaccine' => $travel->vaccine,
+                'urlIdCard' => $travel->url_idcard,
+                'urlIdVaccine' => $travel->url_idvaccine,
+                'idCardNo' => $travel->id_card_no,
+                'approvedAt' => $travel->approved_at ? $travel->approved_at->format('d/m/Y') : null,
+                'createdAt' => $travel->created_at->format('d/m/Y'),
+                'updatedAt' => $travel->updated_at->format('d/m/Y'),
+                'deletedAt' => $travel->deleted_at ?  $travel->deleted_at->format('d/m/Y') : null,
+            ];
+            return $results;
         }
     }
 
